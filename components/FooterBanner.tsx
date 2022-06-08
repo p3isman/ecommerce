6fc9b0d99b5e61e6c8c7fe2049c2ed3fcdaacab8
p3isman <pedro_eisman@outlook.com>
2022-06-08 09:34:52 +0200

    Put largeText1 and largeText2 inside the same h3

--- components/FooterBanner.tsx
+++ components/FooterBanner.tsx
@@ -30,8 +30,9 @@ const FooterBanner = ({
       <div className='banner-desc'>
         <div className='left'>
           <p>{discount}</p>
-          <h3>{largeText1}</h3>
-          <h3>{largeText2}</h3>
+          <h3>
+            {largeText1} {largeText2}
+          </h3>
           <p>{saleTime}</p>
         </div>
         <div className='right'>
